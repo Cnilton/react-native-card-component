d454b33f3bcac8530e8f818bd185db2246897f78
cnilton <conexa.caio.barroso@conexasaude.com.br>
2021-06-06 17:31:59 -0300

    final changes to publish

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import {AppRegistry} from 'react-native';
-import App from './src';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './src';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
-// import CardComponent from './src';
+// AppRegistry.registerComponent(appName, () => App);
+import CardComponent from './src';
 
-// export {CardComponent};
+export {CardComponent};
